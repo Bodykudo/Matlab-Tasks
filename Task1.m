85b70b0ea5f178c2ad90307748a4c97e663c0107
Bodykudo <bodyalsadek2002@gmail.com>
2022-05-27 15:19:39 +0200

    Update Task1.m

--- Task1.m
+++ Task1.m
@@ -2,8 +2,11 @@
 % characteristics of the 3-pole Butterworth low-pass filter with unity gain and
 % normalized frequency at ωc = 1 rad/sec. Print your code, results and figures.
 
+% Define numerator and denominator of the transfer function of the 3-pole Butterworth low-pass filter
 numerator = [0 0 0 1];
 denominator = [1 2 2 1];
+
+% Make the bode diagram of transfer function of the 3-pole Butterworth low-pass filter
 bode(numerator, denominator);
 title("Bode plot for 3-pole Butterworth low-pass filter");
 grid;
